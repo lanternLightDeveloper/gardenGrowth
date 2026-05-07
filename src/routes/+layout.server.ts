1018ae35e98cdb0be4d3026955ce2c904718c3cd
lanternlightdeveloper <joelkrivers@yahoo.com>
2026-05-07 13:04:33 -0700

    test

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,6 +1,6 @@
 // // src/routes/+layout.server.ts
-// export function load({ locals }) {
-// 	return {
-// 		user: locals.user
-// 	};
-// }
+export function load({ locals }) {
+	return {
+		user: locals.user
+	};
+}
