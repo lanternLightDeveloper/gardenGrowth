e18661c542ae8c7b4be2b0d358e336712d5486c2
lanternlightdeveloper <joelkrivers@yahoo.com>
2026-05-08 15:52:47 -0700

    🧪

--- src/routes/admin/entries/+page.server.ts
+++ src/routes/admin/entries/+page.server.ts
@@ -1,18 +1,22 @@
 // src/routes/admin/users/+page.server.ts
-import { db } from '$lib/db';
+import { db } from '$lib/db/index';
 import { users } from '$lib/db/schema';
 import { requireAdmin } from '$lib/db/auth';
-// import { eq, and } from 'drizzle-orm';
 
 export async function load({ locals }) {
 	requireAdmin(locals);
 
-	const all = await db.select({
-		id: users.id,
-		name: users.name,
-		username: users.username,
-		createdAt: users.createdAt,
-		role: users.role
-	});
-	return { users: all };
+	const all = await db
+		.select({
+			id: users.id,
+			name: users.name,
+			username: users.username,
+			createdAt: users.createdAt,
+			role: users.role
+		})
+		.from(users);
+
+	return {
+		users: all
+	};
 }
--- src/routes/admin/entries/new/+page.server.ts
+++ src/routes/admin/entries/new/+page.server.ts
@@ -1,4 +1,4 @@
-import { db } from '$lib/db';
+import { db } from '$lib/db/index';
 import { entries, entryItems } from '$lib/db/schema';
 import { error } from '@sveltejs/kit';
 
