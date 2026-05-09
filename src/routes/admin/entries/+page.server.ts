300ca1f5cfd9207ac3aa42eff19c8dc6df7703ec
lanternlightdeveloper <joelkrivers@yahoo.com>
2026-05-08 18:10:22 -0700

    lol, goofed that one

--- src/routes/admin/entries/+page.server.ts
+++ src/routes/admin/entries/+page.server.ts
@@ -1,7 +1,7 @@
 // src/routes/admin/users/+page.server.ts
 import { db } from '$lib/db/index';
-import { users } from '$lib/db/schema';
-import { requireAdmin, entryItems } from '$lib/db/auth';
+import { users, entryItems } from '$lib/db/schema';
+import { requireAdmin } from '$lib/db/auth';
 
 export async function load({ locals }) {
 	requireAdmin(locals);
